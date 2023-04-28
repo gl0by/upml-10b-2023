71abdb74a437eaa64dfb793abc30c1c6ecee1fcd
gl0by <ehor.tsvietkov@gmail.com>
2023-04-28 10:04:01 +0300

    Create f.py

---
+++ tsvietkov/f.py
@@ -0,0 +1,10 @@
+def input(n, a[]):
+    for i in range(n):
+        l = int(input())
+        a.append(l)
+        
+def check0(b, a[]):
+    for i in range(n):
+        if a[i] == 0:
+            b = true
+
