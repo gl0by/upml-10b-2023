27681f2686b6ed9dd551e36ac6f8733d6aafd899
gl0by <ehor.tsvietkov@gmail.com>
2023-05-12 09:08:40 +0300

    пісь пук как

---
+++ tsvietkov/f.py
@@ -0,0 +1,9 @@
+def inp ( n, a[n] ):
+    for i in range(n):
+        l = int(input())
+        a.append(l)
+
+def check ( n, a[n], b ):
+    for i in range(n):
+        if a[i] == 0:
+            b = true
---
+++ tsvietkov/new.py
@@ -0,0 +1,16 @@
+import inp from f.py
+import check from f.py
+
+n = int(input())
+a=[]
+
+inp(n, a[n])
+
+b = true
+
+check(n, a[n], b)
+
+if b == true:
+    print("yes")
+else:
+    print("no")
